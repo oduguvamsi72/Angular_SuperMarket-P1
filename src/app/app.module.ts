c58bf95ca316600f8eaf641690ec6563d1f2c08a
unknown <oduguvamsi0@gmail.com>
2024-12-20 08:58:35 +0530

    Project Updated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    NavbarComponent,
+    CarouselComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
